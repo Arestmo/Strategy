3a60b517927a3126f653a4365bb7466b5f4cd60d
MateuszT <mateusz@clearmedia.pl>
2020-05-08 16:40:39 +0200

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,75 @@
+﻿namespace Strategy
+{
+    partial class FieldForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ButtonGenerate = new System.Windows.Forms.Button();
+            this.DisplayLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // ButtonGenerate
+            // 
+            this.ButtonGenerate.Location = new System.Drawing.Point(13, 13);
+            this.ButtonGenerate.Name = "ButtonGenerate";
+            this.ButtonGenerate.Size = new System.Drawing.Size(75, 23);
+            this.ButtonGenerate.TabIndex = 0;
+            this.ButtonGenerate.Text = "Generate Field";
+            this.ButtonGenerate.UseVisualStyleBackColor = true;
+            this.ButtonGenerate.Click += new System.EventHandler(this.ButtonGenerate_Click);
+            // 
+            // DisplayLabel
+            // 
+            this.DisplayLabel.AutoSize = true;
+            this.DisplayLabel.Location = new System.Drawing.Point(120, 18);
+            this.DisplayLabel.Name = "DisplayLabel";
+            this.DisplayLabel.Size = new System.Drawing.Size(79, 13);
+            this.DisplayLabel.TabIndex = 1;
+            this.DisplayLabel.Text = "Clicked Button:";
+            // 
+            // FieldForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(357, 372);
+            this.Controls.Add(this.DisplayLabel);
+            this.Controls.Add(this.ButtonGenerate);
+            this.Name = "FieldForm";
+            this.Text = "Strategy";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button ButtonGenerate;
+        private System.Windows.Forms.Label DisplayLabel;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Strategy
+{
+    public partial class FieldForm : Form
+    {
+        Button[,] buttonsTab;
+        int fieldSize;
+        public FieldForm()
+        {
+            InitializeComponent();
+
+        }
+
+        private void ButtonGenerate_Click(object sender, EventArgs e)
+        {
+            fieldSize = 10;
+            Point startXY = new Point(50, 50);
+            Size defaultSize = new Size(50, 50);
+            buttonsTab = new Button[fieldSize, fieldSize];
+            Random rand = new Random();
+            int value_1 = rand.Next(0, fieldSize);
+            int value_2 = rand.Next(0, fieldSize);
+
+
+            for (int i = 0; i < buttonsTab.GetLength(0); i++)
+            {
+                for (int j = 0; j < buttonsTab.GetLength(1); j++)
+                {
+                    Button button = new Button();
+                    button.Name = "Field_" + i + "_" + j;
+                    button.Size = defaultSize;
+                    button.Location = startXY;
+                    button.Click += new EventHandler(ButtonClicked);
+                    startXY.X += 55;
+                    buttonsTab[i, j] = button;
+                    Controls.Add(button);
+                }
+                startXY.Y += 55;
+                startXY.X = 50;
+            }
+            buttonsTab[value_1, value_2].BackColor = Color.Red;
+
+        }
+
+        private void ButtonClicked(object sender, EventArgs e)
+        {
+            if (sender.GetType() == typeof(Button))
+            {
+                Button button = sender as Button;
+                DisplayLabel.Text = button.Name;
+                string[] ButtonNameArr = button.Name.ToString().Split('_');
+                if (checkField(int.Parse(ButtonNameArr[1]), int.Parse(ButtonNameArr[2])))
+                {
+                    buttonsTab[int.Parse(ButtonNameArr[1]), int.Parse(ButtonNameArr[2])].BackColor = Color.Red;
+                }
+            }
+
+        }
+
+        private bool isRed(Button button)
+        {
+            if (button.BackColor == Color.Red) return true;
+            else return false;
+        }
+
+        private bool checkField(int value_i, int value_j)
+        {
+            //Warunek pole i = 0, j=0
+
+            if(value_i == 0 && value_j == 0)
+            {
+                if (isRed(buttonsTab[value_i + 1, value_j]) || isRed(buttonsTab[value_i, value_j + 1])) return true;
+                else return false;
+            }
+            return false;
+
+
+
+
+
+
+
+
+            /*if (value_i < fieldSize && value_j < fieldSize)
+            {
+                if (buttonsTab[value_i, value_j + 1].BackColor == Color.Red ||
+               buttonsTab[value_i, value_j - 1].BackColor == Color.Red ||
+               buttonsTab[value_i + 1, value_j].BackColor == Color.Red ||
+               buttonsTab[value_i - 1, value_j].BackColor == Color.Red)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            else if (value_i == fieldSize || value_j == fieldSize)
+            {
+                if (value_i == fieldSize)
+                {
+                    if (buttonsTab[value_i, value_j + 1].BackColor == Color.Red ||
+                         buttonsTab[value_i, value_j - 1].BackColor == Color.Red ||
+                         buttonsTab[value_i + 1, value_j].BackColor == Color.Red ||
+                          buttonsTab[value_i - 1, value_j].BackColor == Color.Red)
+                    {
+                        return true;
+                    }
+                    else
+                    {
+                        return false;
+                    }
+                }
+            }
+            else if (value_i - 1 < 0 || value_j == fieldSize)
+            {
+                if (buttonsTab[value_i, value_j + 1].BackColor == Color.Red ||
+                buttonsTab[value_i, value_j - 1].BackColor == Color.Red ||
+                buttonsTab[value_i + 1, value_j].BackColor == Color.Red)
+                {
+                    return true;
+                }
+            }
+            else if (value_i == fieldSize || value_j - 1 < 0)
+            {
+                if (buttonsTab[value_i, value_j + 1].BackColor == Color.Red ||
+                buttonsTab[value_i - 1, value_j].BackColor == Color.Red ||
+                buttonsTab[value_i + 1, value_j].BackColor == Color.Red)
+                {
+                    return true;
+                }
+            }*/
+
+        }
+
+        protected override bool ProcessCmdKey(ref Message msg, Keys keyData)
+        {
+            if (keyData == Keys.Escape)
+            {
+                this.Close();
+                return true;
+            }
+            if (keyData == Keys.Space)
+            {
+                DisplayLabel.Text = "SPACJA !";
+            }
+            return base.ProcessCmdKey(ref msg, keyData);
+        }
+    }
+}
